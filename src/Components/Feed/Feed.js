2805eea3c0671d2d1466b8815501fa528cfbc793
Aman-Mandal <002amankumar@gmail.com>
2022-08-02 18:08:56 +0530

    Feed Input Component

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,45 +1,14 @@
 import React from 'react'
 import classes from './Feed.module.css'
-import Avatar from '../Avatar/Avatar'
-import InputOption from '../InputOption/InputOption'
-import { FaImage, FaVideo, FaBriefcase, FaNewspaper } from 'react-icons/fa'
 
-const inputOptions = [
-  { icon: <FaImage />, title: 'Photo', color: '#82B9F1' },
-  { icon: <FaVideo />, title: 'Video', color: '#5F9B41' },
-  { icon: <FaBriefcase />, title: 'Job', color: '#A872E8' },
-  { icon: <FaNewspaper />, title: 'Article', color: '#E16745' },
-]
+import FeedInput from '../FeedInput/FeedInput'
+
+
 
 const Feed = () => {
-  const formSubmitHandler = event => {
-    event.preventDefault()
-  }
-
   return (
     <div className={classes.feed}>
-      <div className={classes['feed-inputContainer']}>
-        <div className={classes['feed-input']}>
-          <Avatar />
-          <form onSubmit={formSubmitHandler}>
-            <input type="text" placeholder="Start a post" />
-          </form>
-        </div>
-
-        <div className={classes['feed-inputOptions']}>
-          {inputOptions.map(option => (
-            <div className={classes['option']} key={option.title}>
-              <div
-                style={{ color: option.color }}
-                className={classes['option-icon']}
-              >
-                {option.icon}
-              </div>
-              <p>{option.title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <FeedInput />
     </div>
   )
 }
---
+++ src/Components/FeedInput/FeedInput.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Avatar from '../Avatar/Avatar'
+import { FaImage, FaVideo, FaBriefcase } from 'react-icons/fa'
+import { MdCalendarViewDay } from 'react-icons/md'
+import classes from './FeedInput.module.css'
+
+const inputOptions = [
+  { icon: <FaImage />, title: 'Photo', color: '#82B9F1' },
+  { icon: <FaVideo />, title: 'Video', color: '#5F9B41' },
+  { icon: <FaBriefcase />, title: 'Job', color: '#A872E8' },
+  { icon: <MdCalendarViewDay />, title: 'Article', color: '#E16745' },
+]
+
+const FeedInput = () => {
+  const formSubmitHandler = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <div className={classes['feed-inputContainer']}>
+      <div className={classes['feed-input']}>
+        <Avatar />
+        <form onSubmit={formSubmitHandler}>
+          <input type="text" placeholder="Start a post" />
+        </form>
+      </div>
+
+      <div className={classes['feed-inputOptions']}>
+        {inputOptions.map(option => (
+          <div className={classes['option']} key={option.title}>
+            <div
+              style={{ color: option.color }}
+              className={classes['option-icon']}
+            >
+              {option.icon}
+            </div>
+            <p>{option.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default FeedInput
