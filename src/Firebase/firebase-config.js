c3b82154b7ae3a8e4ec9d4c936a18b3bdf80928a
Aman-Mandal <002amankumar@gmail.com>
2022-08-04 13:37:46 +0530

    Configured firebase

---
+++ src/Firebase/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBCZsFGR3iq4boJYjxjSSSmaofk3vkA5S0',
+  authDomain: 'linkedin-clone-c4620.firebaseapp.com',
+  projectId: 'linkedin-clone-c4620',
+  storageBucket: 'linkedin-clone-c4620.appspot.com',
+  messagingSenderId: '203970777656',
+  appId: '1:203970777656:web:2cab2d8c7aa8050380cccd',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
